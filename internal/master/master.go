f31b533ce4a32b7c0022c16e51ea5dedda0a39aa
Vaibhav Sijaria <139199971+sophic00@users.noreply.github.com>
2026-04-19 22:31:35 +0530

    Added dead worker eviction

--- internal/master/master.go
+++ internal/master/master.go
@@ -82,9 +82,56 @@ func (m *Master) Run() error {
 		errCh <- grpcServer.Serve(grpcListener)
 	}()
 
+	go m.monitorWorkers()
+
 	err = <-errCh
 	grpcServer.Stop()
 	_ = httpServer.Close()
 
 	return err
+}
+
+// monitorWorkers runs in the background and evicts workers that haven't sent a heartbeat within the timeout.
+func (m *Master) monitorWorkers() {
+	ticker := time.NewTicker(m.workerTimeout / 2)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		m.mu.Lock()
+		now := time.Now()
+		for workerID, worker := range m.workers {
+			if now.Sub(worker.LastHeartbeat) > m.workerTimeout {
+				log.Printf("Worker %s timed out. Last heartbeat: %v. Evicting.", workerID, worker.LastHeartbeat)
+				m.resetWorkerTasksLocked(workerID)
+				delete(m.workers, workerID)
+			}
+		}
+		m.mu.Unlock()
+	}
+}
+
+// resetWorkerTasksLocked resets any tasks that were assigned to a worker that is now considered dead.
+// It assumes m.mu is already locked for writing.
+func (m *Master) resetWorkerTasksLocked(workerID string) {
+	for _, job := range m.jobs {
+		if job.Status == JobStatusCompleted || job.Status == JobStatusAborted {
+			continue
+		}
+
+		for _, mt := range job.MapTasks {
+			if mt.Status == TaskStatusInProgress && mt.WorkerID == workerID {
+				log.Printf("Resetting Map task %s for job %s from dead worker %s", mt.ID, job.ID, workerID)
+				mt.Status = TaskStatusIdle
+				mt.WorkerID = ""
+			}
+		}
+
+		for _, rt := range job.ReduceTasks {
+			if rt.Status == TaskStatusInProgress && rt.WorkerID == workerID {
+				log.Printf("Resetting Reduce task %s for job %s from dead worker %s", rt.ID, job.ID, workerID)
+				rt.Status = TaskStatusIdle
+				rt.WorkerID = ""
+			}
+		}
+	}
 }
